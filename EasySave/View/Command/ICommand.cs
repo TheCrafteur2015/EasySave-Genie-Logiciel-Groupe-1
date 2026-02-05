4f61aa5a802d474415ca809787071e2f8751e88c
Atlonos <gabirocher@icloud.com>
2026-02-05 12:07:37 +0100

    Ajout commandes design pattern Strategy

---
+++ EasySave/View/Command/ICommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave.View.Command
+{
+    public interface ICommand
+    {
+		int GetID();
+
+		string GetI18nKey();
+		
+		void Execute();
+
+	}
+}
---
+++ EasySave/View/Commands/DeleteBackupJobCommand.cs
@@ -0,0 +1,30 @@
+﻿using EasySave.Backup;
+using EasySave.View.Command;
+using EasySave.View.Localization;
+
+namespace EasySave.View.Commands
+{
+	public class DeleteBackupJobCommand : ICommand
+	{
+
+		public string GetI18nKey() => "menu_delete";
+
+		public int GetID() => 5;
+
+		public void Execute()
+		{
+			Console.Clear();
+
+			// Afficher la liste des processus de sauvegarde
+			CommandContext.Instance.ExecuteCommand(4);
+
+			Console.Write(I18n.Instance.GetString("delete_id"));
+
+			if (int.TryParse(Console.ReadLine(), out int id))
+			{
+				string key = BackupManager.GetBM().DeleteJob(id) ? "success" : "failure";
+				Console.WriteLine(I18n.Instance.GetString($"delete_{key}"));
+			}
+		}
+	}
+}

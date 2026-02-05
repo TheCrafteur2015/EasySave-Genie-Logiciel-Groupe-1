4c01ad4aa473eb335ea802dff05d46df2ab7dec1
Atlonos <gabirocher@icloud.com>
2026-02-05 12:35:58 +0100

    Fix de l'accolade manquante

--- EasySave/Extensions/BackupTypeExt.cs
+++ EasySave/Extensions/BackupTypeExt.cs
@@ -11,14 +11,15 @@ namespace EasySave.Extensions
     public static class BackupTypeExt
     {
 
-        public static string GetTranslation(this BackupType type)
+		public static string GetTranslation(this BackupType type)
 		{
-            return I18n.Instance.GetString(type switch
+			return I18n.Instance.GetString(type switch
 			{
 				BackupType.Complete     => "type_complete",
 				BackupType.Differential => "type_differential",
 				BackupType.Incremental  => "not_translated",
 				_                       => "not_translated",
 			});
+		}
     }
 }

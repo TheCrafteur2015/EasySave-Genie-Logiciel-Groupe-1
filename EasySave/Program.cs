7388f4352b65d70ae8bbe596c390904d9f4edd20
GuiZaZotte <enzo.pasquet05@gmail.com>
2026-02-05 10:47:13 +0100

    Commentary done for the main entry point of the application

--- EasySave/Program.cs
+++ EasySave/Program.cs
@@ -8,6 +8,11 @@ namespace EasySave
     /// </summary>
     class Program
     {
+        /// <summary>
+        /// Main method of the project executed at the launch of the application.
+        /// It initialize the console view and manages the global exception of the program.
+        /// </summary>
+        /// <param name="args">Array of arguments passed on the command line at the start of the application.</param>
         static void Main(string[] args)
         {
             var view = new ConsoleView();

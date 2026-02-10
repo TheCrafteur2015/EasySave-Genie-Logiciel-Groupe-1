7cddc686c1d6903bc1e086d45b94c980a09fd8b2
GuiZaZotte <enzo.pasquet05@gmail.com>
2026-02-10 16:59:39 +0100

    Adding JsonLogger classe

---
+++ EasyLog/Logging/JsonLogger.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using System.IO;
+using System.Text;
+using EasyLog.Data;
+
+namespace EasyLog.Logging
+{
+    public class JsonLogger(string path) : AbstractLogger<LogEntry>(path)
+    {
+        private readonly object _lock = new();
+
+        public override string GetExtension() => "json";
+
+        public override void Log(Level level, LogEntry message)
+        {
+            string jsonLine = JsonSerializer.Serialize(message, new JsonSerializerOptions { WriteIndented = true });
+
+            lock (_lock)
+            {
+                File.AppendAllText(path, jsonLine + Environment.NewLine);
+            }
+        }
+
+        public override void LogError(Exception e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

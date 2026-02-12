dd4dc3deb3de882b6600f18926f00348ced13044
ValentinGlairot <valentin.glairot@viacesi.fr>
2026-02-12 08:49:52 +0100

    Ajout de commentaires XML à Program et Main
    Descriptions XML ajoutées à la classe Program et à la méthode Main dans Program.cs pour améliorer la documentation du code. Aucun changement fonctionnel.

--- EasyLog/Program.cs
+++ EasyLog/Program.cs
@@ -1,7 +1,14 @@
 ﻿namespace EasyLog
 {
+    /// <summary>
+    /// Main entry point of the EasyLog application.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Main method of the project executed at the launch of the application.
+        /// </summary>
+        /// <param name="args">Array of arguments passed on the command line at the start of the application.</param>
         public static void Main(string[] args)
         {
 

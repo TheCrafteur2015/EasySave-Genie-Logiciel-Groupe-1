c287b2b60df80c432117187af4c064ce226129e1
ValentinGlairot <valentin.glairot@viacesi.fr>
2026-02-12 08:47:48 +0100

    Ajout de la documentation XML au record LogEntry
    Ajout d'une documentation XML détaillée à l'enregistrement LogEntry pour décrire son objectif et chacun de ses paramètres, améliorant ainsi la lisibilité et la maintenabilité du code. Aucun changement fonctionnel apporté.

--- EasyLog/Data/LogEntry.cs
+++ EasyLog/Data/LogEntry.cs
@@ -1,4 +1,14 @@
 ﻿namespace EasyLog.Data
 {
-	public record LogEntry(int Timestamp, string Name, string Source, string Target, long Size, long ElapsedTime) {}
+    /// <summary>
+    /// Represents a data record for a single log entry.
+    /// This record holds immutable data regarding a specific file transfer operation during a backup.
+    /// </summary>
+    /// <param name="Timestamp">The timestamp indicating when the operation occurred.</param>
+    /// <param name="Name">The name of the backup job associated with this log entry.</param>
+    /// <param name="Source">The full path of the source file.</param>
+    /// <param name="Target">The full path of the target (destination) file.</param>
+    /// <param name="Size">The size of the transferred file (in bytes).</param>
+    /// <param name="ElapsedTime">The duration of the transfer operation (in milliseconds).</param>
+    public record LogEntry(int Timestamp, string Name, string Source, string Target, long Size, long ElapsedTime) { }
 }

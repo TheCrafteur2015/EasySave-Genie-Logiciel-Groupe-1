24baa9f80ceda150b416ad9ae8ef84c9a7dd8317
Atlonos <gabirocher@icloud.com>
2026-02-12 11:15:30 +0100

    Màj classe BackupJob pour sérialisation

--- EasySave/Backup/BackupJob.cs
+++ EasySave/Backup/BackupJob.cs
@@ -1,46 +1,40 @@
+using EasySave.Extensions;
+using System.Diagnostics;
+using System.Text.Json.Serialization;
+
 namespace EasySave.Backup
 {
 	/// <summary>
 	/// Represents a backup job configuration
 	/// </summary>
-	public class BackupJob
+	/// <remarks>
+	/// Initializes a new instance of the BackupJob class with the specified job details and backup type.
+	/// </remarks>
+	/// <param name="Id">The unique identifier for the backup job.</param>
+	/// <param name="Name">The name assigned to the backup job. Cannot be null or empty.</param>
+	/// <param name="SourceDirectory">The path to the source directory to be backed up. Must be a valid directory path.</param>
+	/// <param name="TargetDirectory">The path to the target directory where backups will be stored. Must be a valid directory path.</param>
+	/// <param name="Type">The type of backup to perform for this job.</param>
+	public class BackupJob(int Id, string Name, string SourceDirectory, string TargetDirectory, BackupType Type)
 	{
-		public int Id { get; }
+		public int Id { get; set; } = Id;
 
-		public string Name { get; }
+		public string Name { get; set; } = Name;
 
-		public string SourceDirectory { get; set; }
+		public string SourceDirectory { get; set; } = SourceDirectory;
 
-		public string TargetDirectory { get; set; }
+		public string TargetDirectory { get; set; } = TargetDirectory;
 
-		public readonly BackupType Type;
+		[JsonConverter(typeof(JsonStringEnumConverter))]
+		public BackupType Type { get; set; } = Type;
 
-		public IBackupStrategy Strategy { get; }
+		[JsonIgnore]
+		public IBackupStrategy Strategy { get; } = BackupStrategyFactory.CreateStrategy(Type);
 
-		public DateTime LastExecution { get; private set; }
+		public DateTime LastExecution { get; set; }
 
-		public State State { get; private set; }
-
-		/// <summary>
-		/// Initializes a new instance of the BackupJob class with the specified job details and backup type.
-		/// </summary>
-		/// <param name="id">The unique identifier for the backup job.</param>
-		/// <param name="name">The name assigned to the backup job. Cannot be null or empty.</param>
-		/// <param name="sourceDir">The path to the source directory to be backed up. Must be a valid directory path.</param>
-		/// <param name="targetDir">The path to the target directory where backups will be stored. Must be a valid directory path.</param>
-		/// <param name="type">The type of backup to perform for this job.</param>
-		public BackupJob(int id, string name, string sourceDir, string targetDir, BackupType type)
-		{
-			
-
-			Id              = id;
-			Name            = name;
-			SourceDirectory = sourceDir;
-			TargetDirectory = targetDir;
-			State           = State.Inactive;
-			Type            = type;
-			Strategy        = BackupStrategyFactory.CreateStrategy(type);
-		}
+		[JsonConverter(typeof(JsonStringEnumConverter))]
+		public State State { get; set; } = State.Inactive;
 
 		/// <summary>
 		/// Executes the associated strategy and reports progress through the specified callback.
@@ -66,8 +60,29 @@ namespace EasySave.Backup
 			State = State.Error;
 		}
 
+		public override bool Equals(object? obj)
+		{
+			if (obj == null)
+				return false;
+			if (obj == this)
+				return true;
+			if (obj is BackupJob job)
+			{
+				return job.Id == Id &&
+					job.Name == Name &&
+					job.SourceDirectory == SourceDirectory &&
+					job.TargetDirectory == TargetDirectory &&
+					job.Type == Type &&
+					job.Strategy.GetType() == Strategy.GetType() &&
+					job.State == State;
+			}
+			return false;
+		}
+
+		public override string ToString()
+		{
+			return $"Backup ID: {Id}, name: {Name}, Source: {SourceDirectory}, Destination: {TargetDirectory}, Type: {Type}, Strategy: {Strategy == null}, Last Execution: {LastExecution}, State: {State}";
+		}
 
 	}
-
-	
 }

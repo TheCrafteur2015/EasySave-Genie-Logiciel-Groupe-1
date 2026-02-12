3ca794fa9ff53d7a75d9009201a755c5d5b87306
Atlonos <gabirocher@icloud.com>
2026-02-12 12:41:01 +0100

    Màj signature loggers JSON et XML

--- EasyLog/Logging/JsonLogger.cs
+++ EasyLog/Logging/JsonLogger.cs
@@ -3,34 +3,28 @@ using EasyLog.Data;
 
 namespace EasyLog.Logging
 {
-    public class JsonLogger(string path) : AbstractLogger<LogEntry>(path)
-    {
-        private readonly object _lock = new();
+	public class JsonLogger(string path) : AbstractLogger(path)
+	{
+		private readonly object _lock = new();
 
-        public override string GetExtension() => "json";
-
-        public override void Log(Level level, LogEntry message)
-        {
-            List<LogEntry> logs = new List<LogEntry>();
-            if (File.Exists(LogFile))
-            {
-                string existingContent = File.ReadAllText(LogFile);
-                if (!string.IsNullOrWhiteSpace(existingContent))
-                {
-                    logs = JsonSerializer.Deserialize<List<LogEntry>>(existingContent) ?? new List<LogEntry>();
-                }
-            }
-            logs.Add(message);
-            lock (_lock)
-            {
-                File.WriteAllText(LogFile, JsonSerializer.Serialize(logs, new JsonSerializerOptions { WriteIndented = true }));
-            }
-        }
-
-        public override void LogError(Exception e)
-        {
-            LogEntry errorEntry = new LogEntry(0,e.Message ?? string.Empty,e.StackTrace ?? string.Empty,"",0,0);
-            Log(Level.Error, errorEntry);
-        }
-    }
+		public override string GetExtension() => "json";
+		
+		public override void Log(LogEntry message)
+		{
+			List<LogEntry> logs = new List<LogEntry>();
+			if (File.Exists(LogFile))
+			{
+				string existingContent = File.ReadAllText(LogFile);
+				if (!string.IsNullOrWhiteSpace(existingContent))
+				{
+					logs = JsonSerializer.Deserialize<List<LogEntry>>(existingContent) ?? new List<LogEntry>();
+				}
+			}
+			logs.Add(message);
+			lock (_lock)
+			{
+				File.WriteAllText(LogFile, JsonSerializer.Serialize(logs, new JsonSerializerOptions { WriteIndented = true }));
+			}
+		}
+	}
 }

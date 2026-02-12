ae7b71374a3deb044e31b9cb38bda58734176c73
Atlonos <gabirocher@icloud.com>
2026-02-12 11:24:13 +0100

    Reformatage de code

--- EasySave/Program.cs
+++ EasySave/Program.cs
@@ -1,29 +1,31 @@
 using EasySave.Backup;
 using EasySave.View;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace EasySave
 {
-    /// <summary>
-    /// Main entry point of the EasySave application
-    /// </summary>
-    class Program
-    {
-        /// <summary>
-        /// Main method of the project executed at the launch of the application.
-        /// It initialize the console view and manages the global exception of the program.
-        /// </summary>
-        /// <param name="args">Array of arguments passed on the command line at the start of the application.</param>
-        static void Main(string[] args)
-        {
-            var view = new ConsoleView();
-            try
-            {
-                view.Run(args);
-            }
-            catch (Exception e)
-            {
-                BackupManager.GetLogger().LogError(e);
-            }
-        }
-    }
+	/// <summary>
+	/// Main entry point of the EasySave application
+	/// </summary>
+	public class Program
+	{
+		/// <summary>
+		/// Main method of the project executed at the launch of the application.
+		/// It initialize the console view and manages the global exception of the program.
+		/// </summary>
+		/// <param name="args">Array of arguments passed on the command line at the start of the application.</param>
+		public static void Main(string[] args)
+		{
+			var view = new ConsoleView();
+			try
+			{
+				view.Run(args);
+			}
+			catch (Exception e)
+			{
+				BackupManager.GetLogger().LogError(e);
+			}
+		}
+	}
 }

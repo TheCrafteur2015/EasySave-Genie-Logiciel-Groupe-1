0b4dedca4a4ed01c5433ad63b239372dc3fe3dc7
GuiZaZotte <enzo.pasquet05@gmail.com>
2026-02-23 11:38:24 +0100

    Refactor: use of the primary constructor C# 12.

--- EasyLog/Logging/CompositeLogger.cs
+++ EasyLog/Logging/CompositeLogger.cs
@@ -9,21 +9,10 @@ namespace EasyLog.Logging
     /// Implements the Composite pattern to allow logging to both local files and remote servers
     /// at the same time. This is typically used when the LogMode configuration is set to "Both".
     /// </remarks>
-    public class CompositeLogger : ILogger
+    public class CompositeLogger(ILogger localLogger, ILogger remoteLogger) : ILogger
     {
-        private readonly ILogger _localLogger;
-        private readonly ILogger _remoteLogger;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CompositeLogger"/> class.
-        /// </summary>
-        /// <param name="localLogger">The logger instance for local file writes.</param>
-        /// <param name="remoteLogger">The logger instance for remote server writes.</param>
-        public CompositeLogger(ILogger localLogger, ILogger remoteLogger)
-        {
-            _localLogger = localLogger ?? throw new ArgumentNullException(nameof(localLogger));
-            _remoteLogger = remoteLogger ?? throw new ArgumentNullException(nameof(remoteLogger));
-        }
+        private readonly ILogger _localLogger = localLogger ?? throw new ArgumentNullException(nameof(localLogger));
+        private readonly ILogger _remoteLogger = remoteLogger ?? throw new ArgumentNullException(nameof(remoteLogger));
 
         /// <summary>
         /// Logs an entry to both local and remote destinations.
